ab443680c42314acf78925cdf91a7e3e2d5f8807
rassl <smith.ya-s@yandex.ru>
2023-01-09 19:46:28 +0300

    header

---
+++ src/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from '../nav/Nav';
+import style from './Header.module.css';
+
+export function Header() {
+    return (
+        <div className={style.header}>
+            <Nav/>
+
+        </div>
+    );
+}
+
+
