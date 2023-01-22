7809e75f8418cd9c5df8d903b628e8019cc7285c
rassl <smith.ya-s@yandex.ru>
2023-01-22 19:07:47 +0300

    scss,mixin

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import style from './Project.module.css';
+import style from './Project.module.scss';
 
 type ProjectType = {
     title: string,
-    description: string
+    description: string,
+    style: any
 }
 
 export const Project = (props: ProjectType) => {
     return (
         <div className={style.project}>
-            <div className={style.image}>
+            <div className={style.image} style={props.style}>
                 <div className={style.viewBtn}>Show</div>
             </div>
-            <h3 className={style.projectTitle}> {props.title}</h3>
-            <div className={style.description}>{props.description}</div>
-
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}> {props.title}</h3>
+                <div className={style.description}>{props.description}</div>
+            </div>
         </div>
     );
 }
