0383f61b23f91abc3d6a31af677f5a8f29837bba
rassl <smith.ya-s@yandex.ru>
2023-02-03 17:25:36 +0300

    class header

--- src/classicHeader/ClassicHeader.tsx
+++ src/classicHeader/ClassicHeader.tsx
@@ -1,45 +1,174 @@
-import React, {useEffect, useState} from 'react';
+import React, {MouseEventHandler, useEffect, useState} from 'react';
 import style from './ClassicHeader.module.scss'
+import {Link} from "react-scroll";
+import logo from '../assets/image/logo5.png'
 
 
 export const ClassicHeader = () => {
     const [stickyHeader, setStickyHeader] = useState<boolean>(false);
     const [isNavModalClose, setIsNavModalClose] = useState<boolean>(true);
 
-    const checkScrollTop = () => {
-        let header = document.getElementsByClassName('containerFluid');
-
-        if (header) {
-            if (
-                document.body.scrollTop > 180 ||
-                document.documentElement.scrollTop > 180
-            ) {
-                setStickyHeader(true);
-            } else {
-                setStickyHeader(false);
-            }
-        }
-    };
+    console.log(isNavModalClose)
     useEffect(() => {
+        const checkScrollTop = () => {
+            let header = document.getElementsByClassName('containerFluid');
 
+            if (header) {
+                if (
+                    document.body.scrollTop > 180 ||
+                    document.documentElement.scrollTop > 180
+                ) {
+                    setStickyHeader(true);
+                } else {
+                    setStickyHeader(false);
+                }
+            }
+        };
         window.addEventListener('scroll', checkScrollTop);
 
         return () => {
             window.removeEventListener('scroll', checkScrollTop);
         }
     }, []);
-    console.log(stickyHeader)
+
     return (
         <header id='header' className={style.sticky}>
-            {/*<nav className={style.menu}>*/}
-            <nav className={stickyHeader ?  style.containerFixed  :style.menu  }>
-                <div className={style.containerFluid}>
+            <nav className={stickyHeader ? style.containerFixed : style.menu}>
+                <div className={style.content}>
+                    <div className={style.contentLogo}>
+                        <Link
+                            smooth
+                            duration={500}
+                            style={{cursor: "pointer"}}
+                            className={style.logo}
+                            to="home"
+                            title="Rasul"
+                            onClick={() => {
+                                setIsNavModalClose(true);
+                            }}
+                        >
+                            {"  "}
+                            <img src={logo} alt="Rasul"/>{" "}
+                        </Link>
+
+                    </div>
                     <div className={style.contentMenu}>
-                        <h1>Hello</h1>
+                        <button
+                            onClick={(e) => {
+                                setIsNavModalClose(!isNavModalClose);
+                            }}
+                            className={style.navBtn}
+                            type="button">
+                            <span/>
+                            <span/>
+                            <span/>
+                        </button>
+                        <div id='header-nav' className={isNavModalClose?  style.contentNavRow : style.contentNav}>
+                            <ul>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        spy
+                                        activeClass="active"
+                                        to="home"
+
+                                        onClick={() => {
+
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        spy
+                                        activeClass="active"
+                                        to="about"
+
+                                        onClick={() => {
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        About
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        spy
+                                        activeClass="active"
+                                        to="services"
+                                        onClick={() => {
+
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        What I Do
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        spy
+                                        activeClass="active"
+                                        to="resume"
+
+                                        onClick={() => {
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        Resume
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        spy
+                                        activeClass="active"
+                                        to="portfolio"
+
+                                        onClick={() => {
+
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        Portfolio
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        smooth
+                                        duration={500}
+                                        style={{cursor: "pointer"}}
+                                        activeClass="active"
+                                        to="contact"
+
+                                        onClick={() => {
+
+                                            setIsNavModalClose(false);
+                                        }}
+                                    >
+                                        Contact
+                                    </Link>
+                                </li>
+                            </ul>
+
+                        </div>
 
 
                     </div>
-
                 </div>
 
             </nav>
