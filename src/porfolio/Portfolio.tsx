936db4275267527d917aec819755b358dac64f97
rassl <smith.ya-s@yandex.ru>
2023-02-06 18:46:57 +0300

    portfolio

--- src/porfolio/Portfolio.tsx
+++ src/porfolio/Portfolio.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Portfolio.module.scss'
 import {AnimatePresence} from "framer-motion";
 import {TitleB} from "../common/componentns/titleB/TitleB";
+import { motion } from "framer-motion"
+
+import {projectsData} from "./ProjectsData";
 
 export const Portfolio = () => {
+    const [selectedId, setSelectedId] = useState<string | null> (null)
+
+
+
     return (
-        <section id="portfolio" className={style.section}>
-            <div className={style.container}>
-             <TitleB title={'My Work'} bgTitle={'Portfolio'}/>
+        <>
+            <section id="portfolio" className={style.section}>
+                <div className={style.container}>
+                    <TitleB title={'My Work'} bgTitle={'Portfolio'}/>
+                    <div className={style.contentNone}>
 
-            </div>
 
-        </section>
+                    <div className={style.content}>
+                        {projectsData.length > 0 &&
+                            projectsData.map((project,index)=>(
+                                <div key={index}  className={style.project}>
+                                 <div className={style.projectBox}>
+                                     <div className={style.projectBoxImg}>
+                                         <img src={project.thumbImage} className={style.boxImg} />
+                                         <div className={style.projectBoxOverlay}>
+                                             <a/>
+                                             <div className={style.projectBoxOverlayDetails}>
+                                                 <h5>{project.title}</h5>
+                                                 <span> Category</span>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                                </div>
+                            ))
+                        }
+
+                    </div>
+                    </div>
+                </div>
+
+            </section>
+        </>
     );
 };
 
---
+++ src/porfolio/ProjectsData.js
@@ -0,0 +1,185 @@
+import project1Img from '../assets/images/projects/project-1.jpg'
+import project2Img from '../assets/images/projects/project-2.jpg'
+import project3Img from '../assets/images/projects/project-3.jpg'
+import project4Img from '../assets/images/projects/project-4.jpg'
+import project5Img from '../assets/images/projects/project-5.jpg'
+import project6Img from '../assets/images/projects/project-6.jpg'
+import project7Img from '../assets/images/projects/project-7.jpg'
+
+export const projectsData = [
+    {
+        title: "Project Title 1",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project1Img,
+        sliderImages: [
+            "images/projects/project-1.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+    {
+        title: "Project Title 2",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project2Img,
+        sliderImages: [
+            "images/projects/project-2.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+    {
+        title: "Project Title 3",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project3Img,
+        sliderImages: [
+            "images/projects/project-3.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+    {
+        title: "Project Title 4",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage:project4Img,
+        sliderImages: [
+            "images/projects/project-1.jpg",
+            "images/projects/project-4.jpg",
+        ],
+    },
+    {
+        title: "Project Title 5",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project5Img,
+        sliderImages: [
+            "images/projects/project-1.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+    {
+        title: "Project Title 6",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project6Img,
+        sliderImages: [
+            "images/projects/project-1.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+    {
+        title: "Project Title 7",
+        projectInfo:
+            "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+        client: "Ruby Clinton",
+        technologies: "iOS, HTML5, CSS3, PHP, Java",
+        industry: "Art & Design",
+        date: "July 16, 2019",
+        url: {
+            name: "www.example.com",
+            link: "https://www.example.com",
+        },
+        socialLinks: {
+            facebook: "http://www.facebook.com/",
+            twitter: "http://www.twitter.com/",
+            google: "http://www.google.com/",
+            instagram: "http://www.instagram.com/",
+            mail: "mailto:example@gmail.com",
+        },
+        thumbImage: project7Img,
+        sliderImages: [
+            "images/projects/project-1.jpg",
+            "images/projects/project-5.jpg",
+        ],
+    },
+];
